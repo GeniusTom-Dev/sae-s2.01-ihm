ff57aa5fad668b51f565039f7e568c644d3674a5
GeniusTom <tomeven13@gmail.com>
2023-06-06 01:21:44 +0200

    init files

---
+++ src/main/java/fr/iut/sae/main.java
@@ -0,0 +1,9 @@
+package fr.iut.sae;
+
+import fr.iut.sae.utils.CSVReader;
+
+public class main {
+    public static void main(String[] args) {
+        new CSVReader("data-sisfrance.csv");
+    }
+}
