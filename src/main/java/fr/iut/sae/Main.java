5addb7a36de1e85091e92956d9881d2e366c0d99
GeniusTom <tomeven13@gmail.com>
2023-06-08 18:05:35 +0200

    rename main class to Main

--- src/main/java/fr/iut/sae/Main.java
+++ src/main/java/fr/iut/sae/Main.java
@@ -1,9 +1,8 @@
 package fr.iut.sae;
 
-import fr.iut.sae.view.DashBoard;
 import fr.iut.sae.view.Home;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
         Home.main();
 
