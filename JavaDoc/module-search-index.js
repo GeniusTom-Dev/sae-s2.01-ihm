1c6bc2e02cbac49349ce1d2385d99276c0a021bf
GeniusTom <tomeven13@gmail.com>
2023-06-14 00:24:58 +0200

    Add forget build JavaDocs

---
+++ JavaDoc/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"sae.javafx"}];updateSearchResults();
---
+++ JavaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"sae.javafx","l":"fr.iut.sae"},{"m":"sae.javafx","l":"fr.iut.sae.controlers"},{"m":"sae.javafx","l":"fr.iut.sae.utils"}];updateSearchResults();
